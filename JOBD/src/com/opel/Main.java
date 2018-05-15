67f3970eae056af566ad0837c0fc9d212f200ed9
Maximilan Olbort <max@olbort.org>
2018-05-15 19:27:27 +0200

    Upload

--- JOBD/src/com/opel/Main.java
+++ JOBD/src/com/opel/Main.java
@@ -1,11 +1,11 @@
 package com.opel;
-import Commands.OBDreset;
-import Commands.OBDrpm;
+import Commands.*;
 import com.fazecast.jSerialComm.*;
 
-import java.io.IOException;
+
 import java.io.InputStream;
 import java.io.OutputStream;
+
 
 import static com.fazecast.jSerialComm.SerialPort.FLOW_CONTROL_XONXOFF_IN_ENABLED;
 import static com.fazecast.jSerialComm.SerialPort.FLOW_CONTROL_XONXOFF_OUT_ENABLED;
@@ -41,12 +41,48 @@ public class Main {
         in = obd.getInputStream();
         out = obd.getOutputStream();
 
-        OBDrpm rpmmeter = new OBDrpm();
-        for(int c = 0; c<5000; c++) {
-            rpmmeter.run(in, out);
-            System.out.println(rpmmeter.getResult());
-            Thread.sleep(100);
+
+        OBDcollantTemperature collant = new OBDcollantTemperature();
+
+        OBDDTCcount count = new OBDDTCcount();
+
+        OBDfuelLevel fuel = new OBDfuelLevel();
+
+        OBDfuelRate fuel2 = new OBDfuelRate();
+
+        OBDkmh kmh = new OBDkmh();
+
+        OBDoilTemperature oil = new OBDoilTemperature();
+
+        OBDrpm rpm = new OBDrpm();
+
+        OBDthrottlePosition trottle = new OBDthrottlePosition();
+
+        OBDreset reset = new OBDreset();
+
+        collant.run(in,out);
+        count.run(in,out);
+        fuel.run(in,out);
+        fuel2.run(in,out);
+        kmh.run(in,out);
+        oil.run(in,out);
+        rpm.run(in,out);
+        trottle.run(in,out);
+        System.out.println("Coollanttemp: " + collant.getResult());
+        System.out.println("DTC Count: " + count.getResult());
+        System.out.println("FuelLevel: " + fuel.getResult());
+        System.out.println("FuelRate: " + fuel2.getResult());
+        System.out.println("KMH: " + kmh.getResult());
+        System.out.println("OIL: " + oil.getResult());
+        System.out.println("RPM: " + rpm.getResult());
+        System.out.println("Trootle: " + trottle.getResult());
+        System.out.println("All Done");
+
+        reset.run(in,out);
+        System.out.println(reset.getResult());
+
+
         }
 
     }
-}
+
