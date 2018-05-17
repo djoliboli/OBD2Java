d530aaf717e5188f68fd8cc29c99bedf42d0c3e8
Maximilan Olbort <max@olbort.org>
2018-05-17 11:47:16 +0200

    Upload

--- JOBD/GSON/LiveData.java
+++ JOBD/GSON/LiveData.java
@@ -1,23 +1,8 @@
 package GSON;
 
-public class LiveData{
+public class LiveData {
 
-    private String value = "";
-    private String type = "";
+    public String value = "";
+    public String type = "";
 
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
 }
